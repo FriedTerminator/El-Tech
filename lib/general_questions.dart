1696c109a7afeac17cefa8484985fe367eb1e486
FriedTerminator <nikita.cherepanov02@gmail.com>
2020-04-27 18:55:14 -0700

    I added a search bar and about to create the list for popular  questions

--- lib/general_questions.dart
+++ lib/general_questions.dart
@@ -19,7 +19,85 @@ class GeneralQuestionsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(_pageTitle),),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Previously Answered Popular Questions:",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        title: Text(_pageTitle),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                showSearch(
+                    context: context,
+                    delegate: CustomSearchDelegate());
+              }),
+        ],
+      ),
     );
+  }
+}
+
+class CustomSearchDelegate extends SearchDelegate {
+
+  @override
+  ThemeData appBarTheme(BuildContext context) {
+    assert(context != null);
+    final ThemeData theme = Theme.of(context);
+    assert(theme != null);
+    return theme;
+  }
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    if (query.length < 3) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: Text(
+              "Search term must be longer than two letters.",
+            ),
+          )
+        ],
+      );
+    }
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // This method is called everytime the search term changes. 
+    // If you want to add search suggestions as the user enters their search term, this is the place to do that.
+    return Column();
   }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -50,7 +50,6 @@ class HomeScreen extends StatelessWidget {
                 navigateToHelpPage(context);
               },
             ),
-//            HelpButton(),
           ],
         ),
       ),
